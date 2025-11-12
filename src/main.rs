71bbce14c41fd7ea2b53b7fe7a69020af34df025
Andrea Venuta <venutawebdesign@gmail.com>
2025-11-12 12:36:49 +0100

    Deduplicate CLI subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,14 @@
 use std::path::{Path, PathBuf};
 
-use clap::{Parser, Subcommand};
+use clap::Parser;
 use tracing::{error, info};
 use tracing_subscriber::filter::LevelFilter;
 
 #[derive(Parser)]
 #[clap(name = "amardiscord")]
 struct Cli {
-    #[clap(subcommand)]
-    command: CliCommand,
-}
-
-#[derive(Subcommand)]
-enum CliCommand {
-    /// Build the message database.
-    Build {
-        /// Path to the backup directory (default: `./data`).
-        path: Option<PathBuf>,
-    },
-    /// Serve the content.
-    Serve {
-        /// Path to the backup directory (default: `./data`).
-        path: Option<PathBuf>,
-    },
+    /// Path to the Discord backup directory (default: `./data`).
+    path: Option<PathBuf>,
 }
 
 #[tokio::main]
@@ -35,27 +21,18 @@ async fn main() {
         .with_thread_names(true)
         .init();
 
-    let cli = Cli::parse();
+    let Cli { path } = Cli::parse();
 
-    match cli.command {
-        CliCommand::Build { path } => {
-            if let Err(e) = amardiscord::db::build(path).await {
-                error!("Building database: {e}");
-            }
-        },
-        CliCommand::Serve { path } => {
-            if !Path::new(amardiscord::SQLITE_ARCHIVE_PATH).exists() {
-                info!("Database file doesn't exist. Building it.");
+    if !Path::new(amardiscord::SQLITE_ARCHIVE_PATH).exists() {
+        info!("Database file doesn't exist. Building it.");
 
-                if let Err(e) = amardiscord::db::build(path).await {
-                    error!("Building database: {e}");
-                    return;
-                }
-            }
+        if let Err(e) = amardiscord::db::build(path).await {
+            error!("Building database: {e}");
+            return;
+        }
+    }
 
-            if let Err(e) = amardiscord::serve::serve().await {
-                error!("Server error: {e}");
-            }
-        },
+    if let Err(e) = amardiscord::serve::serve().await {
+        error!("Server error: {e}");
     }
 }
